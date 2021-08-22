d860b7b2a68fc7545803580e9c535902f309789e
Maxim Lisin <lisin.maxim.m@gmail.com>
2021-08-22 11:56:29 +0300

    services_done

--- src/main/java/ru/compareJ/servingwebcontent/CompareMetadata.java
+++ src/main/java/ru/compareJ/servingwebcontent/CompareMetadata.java
@@ -6,15 +6,31 @@ public class CompareMetadata {
     public void metadataCompare(JsonNode node1, JsonNode node2) {
         if(node1.get("metadata").hashCode() != node2.get("metadata").hashCode()) {
             //check description
-            if(node1.get("metadata").get("description").hashCode() != node2.get("metadata").get("description").hashCode()) {
+            if(node1.get("metadata").get("description").hashCode() == node2.get("metadata").get("description").hashCode()) {
                 int hash = node1.get("metadata").get("description").hashCode();
-                Compare.resultCompareFiles.put(hash, ResultCompare.NOTEQUAL);
+
+                Compare.resultCompareFiles.put(hash, ResultCompare.EQUAL);
+            }
+            else {
+                int hash1 = node1.get("metadata").get("description").hashCode();
+                int hash2 = node2.get("metadata").get("description").hashCode();
+
+                Compare.resultCompareFiles.put(hash1, ResultCompare.NOTEQUAL);
+                Compare.resultCompareFiles.put(hash2, ResultCompare.NOTEQUAL);
             }
 
             //check application
-            if(node1.get("metadata").get("application").hashCode() != node2.get("metadata").get("application").hashCode()) {
+            if(node1.get("metadata").get("application").hashCode() == node2.get("metadata").get("application").hashCode()) {
                 int hash = node1.get("metadata").get("application").hashCode();
-                Compare.resultCompareFiles.put(hash, ResultCompare.NOTEQUAL);
+
+                Compare.resultCompareFiles.put(hash, ResultCompare.EQUAL);
+            }
+            else {
+                int hash1 = node1.get("metadata").get("application").hashCode();
+                int hash2 = node2.get("metadata").get("application").hashCode();
+
+                Compare.resultCompareFiles.put(hash1, ResultCompare.NOTEQUAL);
+                Compare.resultCompareFiles.put(hash2, ResultCompare.NOTEQUAL);
             }
         }
     }
--- src/main/java/ru/compareJ/servingwebcontent/CompareServices.java
+++ src/main/java/ru/compareJ/servingwebcontent/CompareServices.java
@@ -4,28 +4,50 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 public class CompareServices {
     public void servicesCompare(JsonNode node1, JsonNode node2) {
-        if(node1.get("services").hashCode() != node2.get("services").hashCode()) {
+        if(node1.get("services").hashCode() == node2.get("services").hashCode()) {
             int hash = node1.get("services").hashCode();
-            Compare.resultCompareFiles.put(hash, ResultCompare.NOTEQUAL);
 
-            //check size
-            if(node1.get("services").size() > node2.get("services").size()) {
-                int value = node1.get("services").size() - 1;
-                int hash1 = node1.get("services").get(value).hashCode();
-                Compare.checkAvailabilityElement.put(hash1, ResultCompare.EXIST);
+            Compare.resultCompareFiles.put(hash, ResultCompare.EQUAL);
+        }
+        else {
+            int hash1 = node1.get("services").hashCode();
+            int hash2 = node2.get("services").hashCode();
+
+            Compare.resultCompareFiles.put(hash1, ResultCompare.NOTEQUAL);
+            Compare.resultCompareFiles.put(hash2, ResultCompare.NOTEQUAL);
+
+            //json file #1
+            for(JsonNode service1 : node1.get("services")) {
+                int count = 0;
+                for(JsonNode service2 : node2.get("services")) {
+                    if(service1.hashCode() == service2.hashCode()) {
+                        ++count;
+                    }
+                }
+                if(count > 0) {
+                    Compare.resultCompareFiles.put(service1.hashCode(), ResultCompare.EQUAL);
+                }
+                else {
+                    Compare.resultCompareFiles.put(service1.hashCode(), ResultCompare.NOTEQUAL);
+                }
             }
-            else if(node1.get("services").size() < node2.get("services").size()) {
-                int value = node2.get("services").size() - 1;
-                int hash2 = node2.get("services").get(value).hashCode();
-                Compare.checkAvailabilityElement.put(hash2,ResultCompare.EXIST);
+
+            //json file #2
+            for(JsonNode service2 : node2.get("services")) {
+                int count = 0;
+                for(JsonNode service1 : node1.get("services")) {
+                    if(service2.hashCode() == service1.hashCode()) {
+                        ++count;
+                    }
+                }
+                if(count > 0) {
+                    Compare.resultCompareFiles.put(service2.hashCode(), ResultCompare.EQUAL);
+                }
+                else {
+                    Compare.resultCompareFiles.put(service2.hashCode(), ResultCompare.NOTEQUAL);
+                }
+
             }
-
-
-//            for(int i = 0; i < node1.get("services").size(); ++i) {
-//                for(int j = 0; j < node2.get("services").size(); ++j) {
-//
-//                }
-//            }
         }
 
 
